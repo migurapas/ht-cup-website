7ac2952e7b3aeec6108bf69c7c6d89eedaa4f28f
migurapas <pascal.migura@volkswagen.de>
2025-12-15 11:56:02 +0100

    Fix for stream placeholder

--- src/Stream.tsx
+++ src/Stream.tsx
@@ -1,24 +1,20 @@
 function Stream() {
-
-
     return (
-        <>
-            <div
-                style={{
-                    width: "1000px",
-                    height: "562.5px",
-                    backgroundColor: "#000",
-                    color: "white",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderRadius: "12px",
-                }}
-            >
-                <h1>Stream</h1>
-            </div>
-
-        </>
+        <div
+            style={{
+                width: "100%",
+                maxWidth: "1000px",
+                aspectRatio: "16 / 9",
+                backgroundColor: "#000",
+                color: "white",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: "12px",
+            }}
+        >
+            <h1>Stream</h1>
+        </div>
     )
 }
 
